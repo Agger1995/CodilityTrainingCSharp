abbea682a0947ae6a9e159620437d8e79f7fd388
agger1995 <christian_agger1995@hotmail.com>
2018-05-12 18:08:13 +0200

    Found perfect solution.
    This a XOR a = 0, which means that you remove all values from the list but the only odd one.

--- TrainingCS/TrainingCS/OddOccurrences.cs
+++ TrainingCS/TrainingCS/OddOccurrences.cs
@@ -6,7 +6,21 @@ namespace TrainingCS
 {
     class OddOccurrences
     {
+        // This is the perfect solution.
+        // The reason is that when you do result ^= Val; you perform an XOR operation.
+        // And if you perform 'a XOR a' you get 0, which basically eliminates all values but the single odd one left.
         public int Solution(int[] A)
+        {
+            int result = 0;
+            foreach (int Val in A)
+            {
+                result ^= Val;
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+
+        public int SolutionWorksButNotOptimized(int[] A)
         {
             if (A.Length == 1)
             {
